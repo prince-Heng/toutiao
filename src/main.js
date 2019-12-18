693476f05d769d062791f1acc4786a953922507c
wangziheng <wangprint@yeah.net>
2019-12-18 20:29:42 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'// 引入elementUi
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less' // 引入默认的css样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)// 手动全局注册
 new Vue({
   router,
   render: h => h(App)
