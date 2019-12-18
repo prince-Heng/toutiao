b2e0b4dcbd19c41ff0357de8c2ad792c825d9003
wangziheng <wangprint@yeah.net>
2019-12-18 23:39:51 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import ElementUI from 'element-ui'// 引入elementUi
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less' // 引入默认的css样式
