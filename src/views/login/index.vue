1dcd3bb71bb1e7e244099a65e645c191ae31b780
wangziheng <wangprint@yeah.net>
2019-12-19 00:44:55 +0800

    黑马头条PC-登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,31 @@
 <template>
-<div>登陆</div>
+<div class="login">
+    <el-card class="box-card">
+        <div class="title">
+            <img src="../../assets/img/logo_index.png" alt="">
+        </div>
+        <el-form  style="margin-top:20px">
+            <el-form-item>
+                <el-input  placeholder="请输入手机号"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input style="width:65%" placeholder="请输入验证码"></el-input>
+                <el-button plain style="float:right">请输入验证码</el-button>
+            </el-form-item>
+            <el-form-item>
+                <el-chackbox>
+                    我已阅读并同意
+                    <el-button type="text">用户协议</el-button>
+                    和
+                    <el-button type="text">隐私条款</el-button>
+                </el-chackbox>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" style="width:100%">点击登录</el-button>
+            </el-form-item>
+        </el-form>
+    </el-card>
+</div>
 </template>
 
 <script>
@@ -8,6 +34,24 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login{
+    background-image: url('../../assets/img/timg (1).jpg');
+    opacity: 0.7;
+    height:100vh;
+    background-size:cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .box-card{
+        height: 350px;
+        width: 440px;
+        .title{
+            text-align: center;
+            img{
+                height: 45px;
+            }
+        }
+    }
+}
 </style>
