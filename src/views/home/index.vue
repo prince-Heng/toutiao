f14f1f5c0896ee981668ad72696182042f49f66e
wangziheng <wangprint@yeah.net>
2019-12-20 18:07:50 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>首页</div>
+<!-- 外层容器 -->
+ <el-container>
+   <!-- 侧边栏容器 -->
+   <el-aside>
+     我是侧边栏
+   </el-aside>
+   <!-- 右侧内容 -->
+   <el-container>
+     <!-- 右侧头部 -->
+      <el-header>我是头</el-header>
+      <!-- 右侧主体 -->
+      <el-main>我是身体</el-main>
+   </el-container>
+ </el-container>
 </template>
 
 <script>
