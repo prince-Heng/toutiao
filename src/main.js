704ac3e3e296c5cee44bd8c8cc823a7fe08c87c8
wangziheng <wangprint@yeah.net>
2019-12-21 11:33:20 +0800

    黑马头条PC-统一全局注册插件的使用方式Vue.use

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router/index'
 import ElementUI from 'element-ui'// 引入elementUi
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less' // 引入默认的css样式
+import Component from './components/home'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 配置默认接口地址
 Vue.prototype.$axios = axios // 将axios赋值给全局的属性，任何位置都可以使用
 Vue.config.productionTip = false
 Vue.use(ElementUI)// 手动全局注册
+Vue.use(Component)// 手动全局注册
 new Vue({
   router,
   render: h => h(App)
