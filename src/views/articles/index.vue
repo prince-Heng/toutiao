3470920287fe879893e672daacbf11bee1006d1f
wangziheng <wangprint@yeah.net>
2019-12-25 01:05:57 +0800

    黑马头条PC-内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card class="articles">
     <bread-crumb slot="header">
       <template slot="title">文章列表</template>
     </bread-crumb>
@@ -17,23 +17,40 @@
           <el-radio :label="3">审核失败</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="频道列表：" >
+      <el-form-item label="频道列表：">
         <!-- 下拉组件 -->
-        <el-select placeholder="请选择"  v-model="searchForm.channel_id" >
+        <el-select placeholder="请选择" v-model="searchForm.channel_id">
           <!-- label为是显示值   value为储存值 -->
           <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="时间选择：">
         <!-- 日期组件 -->
-          <el-date-picker
+        <el-date-picker
           v-model="searchForm.dataRange"
-            type="daterange"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-          ></el-date-picker>
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
       </el-form-item>
     </el-form>
+    <el-row class="articlesTitle" type="flex" align="middle">
+      <span>共找到0条符合条件的内容</span>
+    </el-row>
+    <div class="article-item" v-for="item in 10000" :key="item">
+      <div class="left">
+        <img src="../../assets/img/22.jpg" alt />
+        <div class="info">
+          <span>鬼气--九道流--阿修罗</span>
+          <el-tag class="tag">已发布</el-tag>
+          <span class="data">2019-12-24 17:23:08</span>
+        </div>
+      </div>
+      <div class="right">
+        <i class="el-icon-edit alter">修改</i>
+        <i class="el-icon-delete">删除</i>
+      </div>
+    </div>
   </el-card>
 </template>
 
@@ -44,9 +61,9 @@ export default {
       searchForm: {
         status: 5, // 默认选项，不为0*4的数字
         channel_id: null, // 频道id，默认设置不选
-        dataRange: []// 日期。格式为数组
+        dataRange: [] // 日期。格式为数组
       },
-      channels: []// 接收频道数据
+      channels: [] // 接收频道数据
     }
   },
   methods: {
@@ -58,7 +75,6 @@ export default {
         this.channels = res.data.channels
       })
     }
-
   },
   created () {
     // 实例加载完毕执行请求
@@ -67,5 +83,46 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.articles {
+  .articlesTitle {
+    height: 60px;
+    border-bottom: 1px dashed #999;
+  }
+  .article-item{
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 0;
+    .left{
+      display: flex;
+      img{
+        width: 180px;
+        height: 100px;
+        border-radius: 4px;
+      }
+       .info{
+         height: 100px;
+           display: flex;
+          flex-direction:column;
+          margin-left: 15px;
+          justify-content: space-between;
+          .tag{
+            width: 60px;
+            text-align: center;
+          }
+          .data{
+            color: #999;
+            font-size: 12px;
+          }
+        }
+    }
+    .right{
+      font-size: 12px;
+      color: #666;
+      .alter{
+        margin-right:10px;
+      }
+    }
+  }
+}
 </style>
