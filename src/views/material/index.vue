eda40aa96695c7a079a77bbdf037ccc061ff045b
wangziheng <wangprint@yeah.net>
2019-12-24 10:35:54 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -14,10 +14,10 @@
                             <i class="el-icon-star-on"></i>
                             <i class="el-icon-delete-solid"></i>
                         </el-row>
-
                     </el-card>
                 </div>
             </el-tab-pane>
+
             <el-tab-pane name='collect' label="收藏图片">
                 <div class="img-list">
                     <el-card class="img-card" v-for="item in list" :key="item.id">
@@ -26,6 +26,16 @@
                 </div>
             </el-tab-pane>
         </el-tabs>
+         <el-row type="flex" justify="center">
+                  <el-pagination
+                    background
+                    layout="prev, pager, next"
+                    :total="page.total"
+                    :page-size="page.pageSize"
+                    :current-page="page.currentPage"
+                    @current-change="changePage">
+                  </el-pagination>
+        </el-row>
     </el-card>
 </template>
 
@@ -35,21 +45,38 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []// 接受素材数据
+      list: [], // 接受素材数据
+      page: {
+        total: 0,
+        pageSize: 8,
+        currentPage: 1
+      }
     }
   },
   methods: {
-    //   切换标签页的事件
+    // 切换标签页事件
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()// 当标签页改变，重新获取素材，因为collect是动态值也是通过标签页改变所改编的
+    },
+    // 切换导航栏的事件
     changeTab () {
+      this.page.currentPage = 1// 重置当前页码
       this.getMaterial()// 当标签页改变，重新获取素材，因为collect是动态值也是通过标签页改变所改编的
     },
     //   获取素材
     getMaterial () {
       this.$axios({
         url: '/user/images',
-        promise: { collect: this.activeName === 'collect' }
+        params: {
+          page: this.page.currentPage,
+          per_page: this.page.pageSize,
+          collect: this.activeName === 'collect'
+        }
       }).then(res => {
         this.list = res.data.results// 将图片数据赋值给list
+        this.page.total = res.data.total_count
+        // console.log(this.page.total)
       })
     }
   },
