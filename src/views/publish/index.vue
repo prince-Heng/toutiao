d1fa065812de1e2993510e3b04a6bcd0eca8925e
wangziheng <wangprint@yeah.net>
2019-12-26 17:13:24 +0800

    “黑马头条PC-发表文章-页面结构-简单实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,6 +3,29 @@
       <bread-crumb slot="header">
       <template slot="title">发布文章</template>
       </bread-crumb>
+      <el-form style="margin-left:50px" label='100px'>
+          <el-form-item label="标题">
+              <el-input style="width:60%"></el-input>
+          </el-form-item>
+          <el-form-item label="内容">
+              <el-input  style="width:60%" type="textarea" :rows="3"></el-input>
+          </el-form-item>
+          <el-form-item label="封面">
+              <el-radio-group>
+                  <el-radio>单图</el-radio>
+                  <el-radio>三图</el-radio>
+                  <el-radio>无图</el-radio>
+                  <el-radio>自动</el-radio>
+              </el-radio-group>
+          </el-form-item>
+          <el-form-item label="频道">
+              <el-select></el-select>
+          </el-form-item>
+          <el-form-item>
+              <el-button type="primary">发表</el-button>
+              <el-button>存入草稿</el-button>
+          </el-form-item>
+      </el-form>
   </el-card>
 </template>
 
