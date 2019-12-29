f2429306aa4bba11f1df172b141b7915667512d5
wangziheng <wangprint@yeah.net>
2019-12-29 16:20:18 +0800

    黑马头条PC-账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,6 +3,26 @@
     <bread-crumb slot="header">
         <template slot="title">账户信息</template>
     </bread-crumb>
+    <el-form style="margin-left:50px" label-width="100px">
+      <el-form-item label='用户名'>
+        <el-input style='width:40%'></el-input>
+      </el-form-item>
+      <el-form-item label='简介'>
+        <el-input style='width:40%'></el-input>
+      </el-form-item>
+      <el-form-item label='邮箱'>
+        <el-input style='width:40%'></el-input>
+      </el-form-item>
+      <el-form-item  label='手机号'>
+        <el-input  disabled style='width:40%'></el-input>
+      </el-form-item>
+      <el-form-item >
+        <el-button type="primary">保存信息</el-button>
+      </el-form-item>
+    </el-form>
+    <el-upload class="uploadImg" action="" show-file-list="false">
+      <img src="../../assets/img/22.jpg" alt="">
+    </el-upload>
   </el-card>
 </template>
 
@@ -12,6 +32,15 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+  .uploadImg{
+    position: absolute;
+    top: 200px;
+    right: 300px;
+    img{
+      height: 200px;
+      width: 200px;
+      border-radius: 50%;
+    }
+  }
 </style>
